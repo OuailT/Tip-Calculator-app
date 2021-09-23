95715c62cebe182b8c8ac942124dd313361929d6
tayarthouail <tayarthouail@gmail.com>
2021-09-23 21:06:06 +0200

    refactoring the tip

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
 //[x] grab the ids
-// [] event Listener
-// [] submit func ( calculate amount) (showResultToDOm)
+// [x] event Listener
+// [x] submit func ( calculate amount) (showResultToDOm)
+// [] do not let the buttons be submit 
+// [] function to insert the value of the button
 
-
-//btn part
-const fiveBtn = document.getElementById("five").value;
-const teenBtn = document.getElementById("teen").value;
-const fifteenBtn = document.getElementById("fifteen").value;
-const tFiveBtn = document.getElementById("tFive").value;
-const fiftyBtn = document.getElementById("fifty").value;
-console.log(fiftyBtn)
-// btn finish
-const inputTip = document.getElementById("tip-input");
-
-const tipAmount = document.getElementById("peopleNum");
-const totalEl = document.getElementById("total");
 const submitEl = document.getElementById("submit-btn");
 const form = document.getElementById("form");
 
@@ -22,28 +11,31 @@ const form = document.getElementById("form");
 
 const submitHandler = (e) => {
     e.preventDefault();
-    const billEL = document.getElementById("bill").value; // onsubmit func
+    const billEL = document.getElementById("bill").value; 
     const peopleNum = document.getElementById("peopleNum").value;
-    
-    let billPerPerson = (billEL / peopleNum);
-    let tipPerPeron= ((fiveBtn / 100) * billPerPerson );
-    console.log(tipPerPeron, billPerPerson)
-    tipAmount.innerHTML=`${tipPerPeron}`;
+    const tipAmount = document.getElementById("tip-amount");
+    const totalEl = document.getElementById("total");
+    const inputTip = document.getElementById("tip-input").value;
+    const tipButtons = document.querySelectorAll(".tip-btn");
+    console.log(tipButtons)
 
+    // To calculate the bill per person
+    let billPerPerson = (billEL / peopleNum).toFixed(2);
     
+    // To calculate the tip for per person
+    let tipPerPeron= ((inputTip / 100) * billPerPerson);
+
+    tipAmount.innerHTML=`<span>$</span>${tipPerPeron}`;
+    totalEl.innerHTML=`<span>$</span>${billPerPerson}`;
+
+    Array.from(tipButtons).forEach((btn)=> {
+        btn.addEventListener("click", (e)=> {
+            const value = e.target.value;
+            
+        })
+    })
 
 }
-
-
-
-
-
-
-
-
-
-
-
 
 //Event Listener
 form.addEventListener("submit", submitHandler);
